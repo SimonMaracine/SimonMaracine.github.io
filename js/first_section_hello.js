cc13c07af956f2c243f052c8f48b36d0ee46ff20
SimonMaracine <simonmaracine@gmail.com>
2022-03-25 09:36:57 +0200

    Improve first section text and hopefully fix background image jump on mobile

--- js/first_section_hello.js
+++ js/first_section_hello.js
@@ -12,3 +12,12 @@ window.onload = function() {
         hello2.classList.add("fade-in");
     }, 2000);
 }
+
+const firstSectionDiv = document.getElementsByClassName("first-section-content")[0];
+
+function resizeBackground() {
+    firstSectionDiv.height = window.height;
+}
+
+window.resizeBackground(resizeBackground);
+resizeBackground();
