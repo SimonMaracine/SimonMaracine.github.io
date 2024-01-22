2f398711b0bfa32441d72793faae7de5c450bec2
SimonMaracine <simonmaracine@gmail.com>
2024-01-22 14:49:36 +0200

    Begin working on a dark theme mode

--- js/navigation_bar.js
+++ js/navigation_bar.js
@@ -1,3 +1,23 @@
+function afterLoad() {
+    const themeButton = document.getElementById("theme-button");
+
+    themeButton.addEventListener("click", () => {
+        document.body.classList.toggle("dark-theme");
+
+        const theme = document.body.classList.contains("dark-theme") ? "dark" : "light";
+        localStorage.setItem("theme", theme);
+    });
+
+    const currentTheme = localStorage.getItem("theme");
+
+    if (currentTheme === "dark") {
+        document.body.classList.add("dark-theme");
+        console.log("DARK THEME");
+    } else {
+        console.log("light theme");
+    }
+}
+
 $(function() {
-    $("#navigation-bar").load("/html/components/navigation_bar.html");
+    $("#navigation-bar").load("/html/components/navigation_bar.html", afterLoad);
 });
