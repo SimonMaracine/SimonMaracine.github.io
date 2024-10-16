564b89bb14750dd10d160ffc79110fba4c5a5228
SimonMaracine <simonmaracine@gmail.com>
2024-10-16 23:39:40 +0300

    Work on the blog archive

--- scripts/build.py
+++ scripts/build.py
@@ -6,10 +6,40 @@ import json
 import make_html
 
 
-def article_template(title: str, date: str, preview: str, article_name: str) -> str:
+def stringify_month(month: int) -> str:
+    match month:
+        case 1:
+            return "January"
+        case 2:
+            return "February"
+        case 3:
+            return "March"
+        case 4:
+            return "April"
+        case 5:
+            return "May"
+        case 6:
+            return "June"
+        case 7:
+            return "July"
+        case 8:
+            return "August"
+        case 9:
+            return "September"
+        case 10:
+            return "October"
+        case 11:
+            return "November"
+        case 12:
+            return "December"
+        case _:
+            raise RuntimeError("Invalid month")
+
+
+def pages_article_template(title: str, date: str, preview: str, article_name: str) -> str:
     return \
 f"""
-<div class="pages-item">
+<div class="pages-article">
     <p class="page-title" lang="ro">{title}</p>
     <p>{date}</p>
     <p lang="ro">{preview}...</p>
@@ -20,11 +50,20 @@ f"""
 """.strip()
 
 
-def pagination_template(index: int, active: bool) -> str:
+def pages_pagination_template(index: int, active: bool) -> str:
     return \
 f"""
 <li {'class="active"' if active else ''}>
-    <a class="page-link" href="/pages-archive-{index}.html">{index}</a>
+    <a href="/pages-archive-{index}.html">{index}</a>
+</li>
+""".strip()
+
+
+def simon_says_article_template(title: str, date: str, article_name: str) -> str:
+    return \
+f"""
+<li class="simon-says-article">
+    <a class="outlined-link" href="/simon-says/{article_name}.html">{title} | {date}</a>
 </li>
 """.strip()
 
@@ -41,13 +80,11 @@ def build_index():
             article_metadata = json.load(file)
 
         date = article_metadata["date"]
-        last_modified = article_metadata["last-modified"]
-        keywords = article_metadata["keywords"]
 
         latest_articles_content.append(
-            article_template(
+            pages_article_template(
                 article_metadata["title"],
-                f"{date["month"]} {date["day"]}, {date["year"]}",
+                f"{stringify_month(date["month"])} {date["day"]}, {date["year"]}",
                 article_metadata["preview"],
                 article
             )
@@ -86,19 +123,17 @@ def build_pages_archive():
                 article_metadata = json.load(file)
 
             date = article_metadata["date"]
-            last_modified = article_metadata["last-modified"]
-            keywords = article_metadata["keywords"]
 
             articles_in_page_content.append(
-                article_template(
+                pages_article_template(
                     article_metadata["title"],
-                    f"{date["month"]} {date["day"]}, {date["year"]}",
+                    f"{stringify_month(date["month"])} {date["day"]}, {date["year"]}",
                     article_metadata["preview"],
                     article
                 )
             )
 
-        pagination_content = [pagination_template(i + 1, index == i) for i in range(article_pages)]
+        pagination_content = [pages_pagination_template(i + 1, index == i) for i in range(article_pages)]
 
         make_html.make_html(
             "../html/templates/pages-archive.html",
@@ -113,7 +148,7 @@ def build_pages_archive():
         )
 
 
-def build_pages():
+def build_pages_articles():
     with open("../articles/pages/ALL.json", "r") as file:
         articles = json.load(file)
 
@@ -133,19 +168,57 @@ def build_pages():
                 make_html.Macro("M_BACK_TO_TOP_BUTTON", "../html/back-to-top-button.html", True),
                 make_html.Macro("M_COPYRIGHT", "../html/copyright.html", True),
                 make_html.Macro("M_TITLE", article_metadata["title"], False),
-                make_html.Macro("M_DATE", f"{date["month"]} {date["day"]}, {date["year"]}", False),
+                make_html.Macro("M_DATE", f"{stringify_month(date["month"])} {date["day"]}, {date["year"]}", False),
                 make_html.Macro("M_KEYWORDS", ", ".join(keywords), False),
-                make_html.Macro("M_LAST_MODIFIED", f"{last_modified["month"]} {last_modified["day"]}, {last_modified["year"]}", False),
+                make_html.Macro("M_LAST_MODIFIED", f"{stringify_month(last_modified["month"])} {last_modified["day"]}, {last_modified["year"]}", False),
                 make_html.Macro("M_CONTENTS", f"../html/articles/pages/{article}.html", True)
             ]
         )
+
+
+def build_simon_says_archive():
+    with open("../articles/simon-says/ALL.json", "r") as file:
+        articles = json.load(file)
+
+    for article in articles:
+        with open(f"../articles/simon-says/{article}.json", "r") as file:
+            article_metadata = json.load(file)
+
+        articles_content: list[str] = []
+
+        date = article_metadata["date"]
+
+        articles_content.append(
+            simon_says_article_template(
+                article_metadata["title"],
+                f"{stringify_month(date["month"])} {date["day"]}, {date["year"]}",
+                article
+            )
+        )
+
+    make_html.make_html(
+        "../html/templates/simon-says-archive.html",
+        f"../simon-says-archive.html",
+        [
+            make_html.Macro("M_NAVIGATION_BAR", "../html/navigation-bar.html", True),
+            make_html.Macro("M_BACK_TO_TOP_BUTTON", "../html/back-to-top-button.html", True),
+            make_html.Macro("M_COPYRIGHT", "../html/copyright.html", True),
+            make_html.Macro("M_ARTICLES", "\n".join(articles_content), False)
+        ]
+    )
+
+
+def build_simon_says_articles():
+    pass
 
 
 def main(args: list[str]) -> int:
     try:
         build_index()
         build_pages_archive()
-        build_pages()
+        build_pages_articles()
+        build_simon_says_archive()
+        build_simon_says_articles()
     except Exception as err:
         print(f"An error occurred building the pages: {err}", file=sys.stderr)
         return 1
